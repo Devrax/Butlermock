f6b6f3f0a02bdd7dd526b4e5e4498b35c23a6994
Rafael Mejia <devrafx@gmail.com>
2023-08-16 08:19:43 -0400

    feat: Add loading state when fetching a transformation
    chore: Remove button interactivity when disabled

--- islands/EditorView.tsx
+++ islands/EditorView.tsx
@@ -3,7 +3,11 @@ import EditorResponse from "../components/EditorResponse.tsx";
 declare var theEditor: any;
 declare var previewer: any;
 
-const fetchTransformation = async (value: string, valueForAny: string, mustReturn = '') => {
+const fetchTransformation = async (
+  value: string,
+  valueForAny: string,
+  mustReturn = "",
+) => {
   const json = await fetch(new URL("/api/typemocker", location.origin).href, {
     method: "POST",
     headers: {
@@ -12,47 +16,61 @@ const fetchTransformation = async (value: string, valueForAny: string, mustRetur
     body: JSON.stringify({
       value,
       valueForAny,
-      mustReturn
+      mustReturn,
     }),
   });
   return JSON.stringify(await json.json(), null, 2);
 };
 
 export default function EditorView() {
-  const codeToShow = useSignal('');
-  const valueForAny = useSignal<string>('null');
+  const isLoading = useSignal(false);
+  const codeToShow = useSignal("");
+  const valueForAny = useSignal<string>("null");
   const valuesForAny = [
-    'null',
-    'true',
-    'false',
+    "null",
+    "true",
+    "false",
     "{}",
-    'undefined',
+    "undefined",
     "lorem ipsum",
-    '0',
-    '9'
+    "0",
+    "9",
   ];
 
   const fetchAndShow = async () => {
-    const code = await fetchTransformation(theEditor.getValue(), valueForAny.value);
-    codeToShow.value = code;
-  }
+    try {
+      isLoading.value = true;
+      const code = await fetchTransformation(
+        theEditor.getValue(),
+        valueForAny.value,
+      );
+      codeToShow.value = code;
+    } finally {
+      isLoading.value = false;
+    }
+  };
 
   const fetchAndCopy = async () => {
     navigator.clipboard.writeText(previewer.getValue());
-  }
+  };
 
   const cleanEditors = () => {
-    theEditor.setValue('');
-    previewer.setValue('');
-  }
+    theEditor.setValue("");
+    previewer.setValue("");
+  };
 
   return (
     <>
       <article>
         <section class="flex justify-center items-center gap-5">
           <div class="bg-[white] text-center rounded h-[50px] flex items-center p-2">
-            <label for="Any-values">value for any:  </label>
-            <select name="any-values" id="Any-values" class="text-center" onChange={(e) => valueForAny.value = (e.target as any).value} >
+            <label for="Any-values">value for any:</label>
+            <select
+              name="any-values"
+              id="Any-values"
+              class="text-center"
+              onChange={(e) => valueForAny.value = (e.target as any).value}
+            >
               {valuesForAny.map((v, index) => (
                 <option
                   value={v}
@@ -67,26 +85,59 @@ export default function EditorView() {
             <button
               onClick={() => fetchAndShow()}
               title="Show me mock"
+              disabled={isLoading.value}
               class="active:bg-monaco-950 hover:bg-monaco-800 hover:fill-monaco-50 hover:text-monaco-50 rounded-full p-1"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
-                />
-              </svg>
+              {isLoading.value
+                ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="w-6 h-6"
+                    viewBox="0 0 135 135"
+                  >
+                    <path d="M67.447 58c5.523 0 10-4.477 10-10s-4.477-10-10-10-10 4.477-10 10 4.477 10 10 10zm9.448 9.447c0 5.523 4.477 10 10 10 5.522 0 10-4.477 10-10s-4.478-10-10-10c-5.523 0-10 4.477-10 10zm-9.448 9.448c-5.523 0-10 4.477-10 10 0 5.522 4.477 10 10 10s10-4.478 10-10c0-5.523-4.477-10-10-10zM58 67.447c0-5.523-4.477-10-10-10s-10 4.477-10 10 4.477 10 10 10 10-4.477 10-10z">
+                      <animateTransform
+                        attributeName="transform"
+                        type="rotate"
+                        from="0 67 67"
+                        to="-360 67 67"
+                        dur="2.5s"
+                        repeatCount="indefinite"
+                      />
+                    </path>
+                    <path d="M28.19 40.31c6.627 0 12-5.374 12-12 0-6.628-5.373-12-12-12-6.628 0-12 5.372-12 12 0 6.626 5.372 12 12 12zm30.72-19.825c4.686 4.687 12.284 4.687 16.97 0 4.686-4.686 4.686-12.284 0-16.97-4.686-4.687-12.284-4.687-16.97 0-4.687 4.686-4.687 12.284 0 16.97zm35.74 7.705c0 6.627 5.37 12 12 12 6.626 0 12-5.373 12-12 0-6.628-5.374-12-12-12-6.63 0-12 5.372-12 12zm19.822 30.72c-4.686 4.686-4.686 12.284 0 16.97 4.687 4.686 12.285 4.686 16.97 0 4.687-4.686 4.687-12.284 0-16.97-4.685-4.687-12.283-4.687-16.97 0zm-7.704 35.74c-6.627 0-12 5.37-12 12 0 6.626 5.373 12 12 12s12-5.374 12-12c0-6.63-5.373-12-12-12zm-30.72 19.822c-4.686-4.686-12.284-4.686-16.97 0-4.686 4.687-4.686 12.285 0 16.97 4.686 4.687 12.284 4.687 16.97 0 4.687-4.685 4.687-12.283 0-16.97zm-35.74-7.704c0-6.627-5.372-12-12-12-6.626 0-12 5.373-12 12s5.374 12 12 12c6.628 0 12-5.373 12-12zm-19.823-30.72c4.687-4.686 4.687-12.284 0-16.97-4.686-4.686-12.284-4.686-16.97 0-4.687 4.686-4.687 12.284 0 16.97 4.686 4.687 12.284 4.687 16.97 0z">
+                      <animateTransform
+                        attributeName="transform"
+                        type="rotate"
+                        from="0 67 67"
+                        to="360 67 67"
+                        dur="8s"
+                        repeatCount="indefinite"
+                      />
+                    </path>
+                  </svg>
+                )
+                : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+                    />
+                  </svg>
+                )}
             </button>
             <button
               onClick={() => fetchAndCopy()}
               title="Just copy, I trust on butlermock"
+              disabled={isLoading.value}
               class="active:bg-monaco-950 hover:bg-monaco-800 hover:fill-monaco-50 hover:text-monaco-50 rounded-full p-1"
             >
               <svg
@@ -107,6 +158,7 @@ export default function EditorView() {
             <button
               title="Clean the code"
               onClick={() => cleanEditors()}
+              disabled={isLoading.value}
               class="active:bg-monaco-950 hover:bg-monaco-800 hover:fill-monaco-50 hover:text-monaco-50 rounded-full p-1"
             >
               <svg
@@ -124,7 +176,6 @@ export default function EditorView() {
                 />
               </svg>
             </button>
-
           </div>
         </section>
       </article>
@@ -132,8 +183,6 @@ export default function EditorView() {
         <section id="container" style="width: 50%; height: 100%;"></section>
         <EditorResponse codeToShow={codeToShow.value} />
       </article>
-
-
 
       <script src="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs/loader.min.js">
       </script>
