430db6459d6748ff03a5f7fcc1d1115fbb25333d
Rafael Mejia <devrafx@gmail.com>
2023-08-19 19:31:49 -0400

    feat: Add Buymeacoffe widget!

--- routes/index.tsx
+++ routes/index.tsx
@@ -42,6 +42,8 @@ export default function Home() {
 
           <EditorView />
         </main>
+
+        <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="devrafx" data-description="Support me on Buy me a coffee!" data-message="Thanks for using Butlermock!" data-color="#888888" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
       </body>
     </>
   );
