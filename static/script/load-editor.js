3603ba73f21ea352de865d173cdfe1c810337935
Rafael Mejia <devrafx@gmail.com>
2023-08-21 15:28:06 -0400

    chore: Format interface example

--- static/script/load-editor.js
+++ static/script/load-editor.js
@@ -1,43 +1,47 @@
 // require is provided by loader.min.js.
 var theEditor = null;
 var previewer = null;
-require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs' }});
+require.config({
+  paths: {
+    vs: "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs",
+  },
+});
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   require(["vs/editor/editor.main"], () => {
-    theEditor = monaco.editor.create(document.getElementById('container'), {
-      value: `interface Example {
-        avatar_url: string;
-        gravatar_id: string;
-        url: string;
-        name: string;
-        blog: string;
-        location: string;
-        email?: any;
-        hireable: boolean;
-        bio: string;
-        twitter_username?: any;
-        following: number;
-        created_at: Date;
-      }`,
-      language: 'typescript',
-      theme: 'vs-dark',
+    theEditor = monaco.editor.create(document.getElementById("container"), {
+      language: "typescript",
+      theme: "vs-dark",
       autoIndent: true,
       formatOnPaste: true,
       minimap: {
-        enabled: false
-      }
+        enabled: false,
+      },
     });
-  
-    previewer = monaco.editor.create(document.getElementById('preview-code'), {
+
+    theEditor.setValue(`interface Example {
+    avatar_url: string;
+    gravatar_id: string;
+    url: string;
+    name: string;
+    blog: string;
+    location: string;
+    email?: any;
+    hireable: boolean;
+    bio: string;
+    twitter_username?: any;
+    following: number;
+    created_at: Date;
+}`);
+
+    previewer = monaco.editor.create(document.getElementById("preview-code"), {
       value: ``,
-      language: 'json',
-      theme: 'vs-dark',
+      language: "json",
+      theme: "vs-dark",
       readOnly: true,
       minimap: {
-          enabled: false
-      }
-  })
+        enabled: false,
+      },
+    });
   });
-  
-})
+});
